1b4fb2a59e92e53fe917f33c5aa81d299358bb49
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-01-18 20:57:29 +0530

    Added solution - LeetHub

---
+++ Palindromic patitioning - GFG/palindromic-patitioning.cpp
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+// Initial Template for c++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    bool ispalindrome(string &str, int i, int j){
+        if(i>=j) return true;
+        while(i<=j){
+            if(str[i]!=str[j]) return false;
+            i++;j--;
+        }
+        return true;
+    }
+    
+    int dp[502][502];
+    int solve(string &str,int i, int j){
+        if(i>=j) return 0;
+        
+        if(ispalindrome(str,i,j)) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int mini = INT_MAX;
+        for(int k=i;k<j;k++){
+            int temp = solve(str,i,k) + solve(str,k+1,j) + 1;
+            mini = min(mini,temp);
+        }
+        return dp[i][j] = mini;
+    }
+    int palindromicPartition(string str)
+    {
+        memset(dp,-1,sizeof(dp));
+        return solve(str,0,str.length()-1);
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string str;
+        cin>>str;
+        
+        Solution ob;
+        cout<<ob.palindromicPartition(str)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
