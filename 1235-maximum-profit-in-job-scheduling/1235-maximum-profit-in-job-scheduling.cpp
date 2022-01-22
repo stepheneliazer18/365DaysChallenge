55dfb54d14febafbc6049fa8d5094055821a2172
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-01-22 16:03:19 +0530

    Time: 132 ms (86.00%), Space: 47.1 MB (95.66%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
+    struct Job{ 
+        int start;	 // Job Id 
+        int end; // Deadline of job 
+        int profit; // Profit if job is over before or on deadline 
+    };
+    struct{
+        bool operator()(Job j1, Job j2)const{
+            return j1.end < j2.end;
+        }
+    }cmp;
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         int n = profit.size();
         
-        vector<vector<int>> vec;
-        for(int i=0;i<n;i++) vec.push_back({endTime[i],startTime[i],profit[i]});
-        sort(vec.begin(),vec.end());
-        
-        vector<int> dp(n);
-        int res = 0;
-        
+        struct Job job[n];
         for(int i=0;i<n;i++){
-            
-            dp[i] = vec[i][2];
-            
-            if(i==0){
-                res = max(res,dp[i]);
-                continue;
-            }
-            
-            dp[i] = max(dp[i],dp[i-1]);
-            int cur = vec[i][1];
-            for(int j=i-1;j>=0;j--){
-                if(vec[j][0]<=cur){
-                    dp[i] = max(dp[i],dp[j]+vec[i][2]);
-                    break;
-                }  
-            }
-            res = max(res,dp[i]);
+            job[i].start = startTime[i];
+            job[i].end = endTime[i];
+            job[i].profit = profit[i];
         }
-        return res;
+        sort(job,job+n,cmp);
+        vector<int> vec;
+        int maxi = 0;
+        for(int i=0;i<n;i++)vec.push_back(job[i].profit);
+        for(int i=1;i<n;i++){
+            vec[i] = max(vec[i],vec[i-1]);
+            for(int j=i-1;j>=0;j--){
+                if(job[j].end<=job[i].start){
+                    vec[i] = max(vec[i],vec[j] + job[i].profit);
+                    break;
+                }
+            }
+            maxi = max(maxi,vec[i]);
+        }
+        return maxi;
     }
 };
