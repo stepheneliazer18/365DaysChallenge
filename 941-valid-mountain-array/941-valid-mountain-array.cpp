87ae01fd213225bd8f0a2dab4f7a2dbad2c1014c
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-01-25 16:06:29 +0530

    Time: 36 ms (43.22%), Space: 22.5 MB (48.39%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.cpp
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -4,12 +4,12 @@ public:
         int n = arr.size();
         if(n<3) return false;
         
-        int cnt = 0;
+        bool flag = false;
         for(int i=1;i<n-1;i++){
             if(arr[i-1]==arr[i] || arr[i]==arr[i+1] ||
                 arr[i-1]>arr[i] && arr[i]<arr[i+1])return false;
-            if(arr[i-1]<arr[i] && arr[i]>arr[i+1]) cnt++;
+            if(arr[i-1]<arr[i] && arr[i]>arr[i+1] && !flag) flag = true;
         }
-        return cnt==1;
+        return flag;
     }
 };
