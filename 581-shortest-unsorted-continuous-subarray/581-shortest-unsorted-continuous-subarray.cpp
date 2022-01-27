32285a4e7a8da79e2bea368bc4ccba0675f65c8f
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-01-27 15:31:16 +0530

    Time: 44 ms (41.01%), Space: 26.5 MB (86.08%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -3,21 +3,14 @@ public:
     int findUnsortedSubarray(vector<int>& nums) {
         int n = nums.size();
         if(n==1) return 0;
-
         
         int l=0;
         int r = n-1;
-        for(l=0;l<n-1;l++){
-            if(nums[l] > nums[l+1])break;
-        }
-        for(r=n-1;r>0;r--){
-            if(nums[r] < nums[r-1])break;
-        }
-        
+        for(l=0;l<n-1;l++)  if(nums[l] > nums[l+1])break; 
+        for(r=n-1;r>0;r--) if(nums[r] < nums[r-1])break; 
         if(l==n || r==0) return 0;
         
-        cout<<"l = "<<l<<" r = "<<r<<endl;
-        
+
         int mini = INT_MAX;
         int maxi = INT_MIN;
         for(int i=l;i<=r;i++){
@@ -25,33 +18,11 @@ public:
             maxi = max(maxi, nums[i]);
         }
         
-        cout<<"mini = "<<mini<<" maxi = "<<maxi<<endl;
-        
-        l = 0;
-        r = n-1;
-        for(l=0;l<n;l++){
-            cout<<mini<<" > "<<nums[l]<<endl;
-            if(mini < nums[l]) break;
-        }
-        for(r = n-1;r>=0;r--){
-            cout<<maxi<<" > "<<nums[r]<<endl;
-            if(maxi > nums[r]) break;
-        }
-        
-        cout<<"l = "<<l<<" r = "<<r<<endl;
+
+        for(l=0;l<n;l++) if(mini < nums[l]) break;
+        for(r = n-1;r>=0;r--) if(maxi > nums[r]) break;
 
         if(l>r) return 0;
         return r-l+1;
     }
 };
-/*
-
-[2,6,4,8,10,9,15]
-[1,2,3,4]
-[1]
-[2,1]
-[1,2,3,3,3]
-[1,3,2,2,2]
-[2,3,3,2,4]
-
-*/
