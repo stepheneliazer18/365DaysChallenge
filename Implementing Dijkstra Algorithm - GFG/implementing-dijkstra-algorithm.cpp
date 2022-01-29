ca8b15b80193b09d72eb66aafdc1cbd6b5b42f88
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-01-29 19:22:46 +0530

    Added solution - LeetHub

---
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -0,0 +1,71 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+{
+	public:
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S){
+        vector<int> dis(V,INT_MAX);
+        dis[S] = 0;
+        
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> q;
+        q.push({0,S});
+        
+        while(!q.empty()){
+            int node = q.top().second;
+            q.pop();
+            
+            for(auto &it: adj[node]){
+                int v = it[0];
+                int wt = it[1];
+                if(dis[node]+wt < dis[v]){
+                    dis[v] = dis[node] + wt;
+                    q.push({dis[v],v});
+                }
+            }
+        }
+        return dis;
+    }
+};
+
+
+// { Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        int S;
+        cin>>S;
+        
+        Solution obj;
+    	vector<int> res = obj.dijkstra(V, adj, S);
+    	
+    	for(int i=0; i<V; i++)
+    	    cout<<res[i]<<" ";
+    	cout<<endl;
+    }
+
+    return 0;
+}
+
+  // } Driver Code Ends
