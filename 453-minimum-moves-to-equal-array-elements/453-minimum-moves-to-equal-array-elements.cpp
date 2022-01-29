ea1e5da373db0ab784d67dc993c80c4e0cab601f
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-01-29 12:30:34 +0530

    Time: 67 ms (26.67%), Space: 28.3 MB (83.08%) - LeetHub

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
@@ -1,3 +1,26 @@
+//              Brute Force -   TLE
+// class Solution {
+// public:
+//     bool isequal(vector<int>& nums){
+//         for(int i=0;i<nums.size()-1;i++){
+//             if(nums[i]!=nums[i+1])return false;
+//         }
+//         return true;
+//     }
+//     int minMoves(vector<int>& nums) {
+//         int n = nums.size();
+        
+//         int cnt = 0;
+//         while(!isequal(nums)){
+//             sort(nums.begin(),nums.end());
+//             for(int i=0;i<n-1;i++) nums[i]++;
+//             cnt++;
+//         }
+//         return cnt;
+//     }
+// };
+
+//              Optimized - O(n) - AC
 class Solution {
 public:
     int minMoves(vector<int>& nums) {
