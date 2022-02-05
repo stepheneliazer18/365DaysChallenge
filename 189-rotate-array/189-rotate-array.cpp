8eaf602f5893b092598474a9c8dae1c851116e12
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-02-05 23:13:53 +0530

    Time: 57 ms (22.63%), Space: 25 MB (80.50%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,17 +1,9 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
         k = k%nums.size();
-        
-        vector<int> vec;
-        
-        for(int i=n-k;i<n;i++){
-            vec.push_back(nums[i]);
-        }
-        for(int i=0;i<n-k;i++){
-            vec.push_back(nums[i]);
-        }
-        nums = vec;
+        reverse(nums.begin(),nums.end()-k);
+        reverse(nums.end()-k,nums.end());
+        reverse(nums.begin(),nums.end());
     }
 };
