a3fa2826be66e6710bb046d91af3ffccc13c6501
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-02-05 16:39:58 +0530

    Time: 664 ms (5.02%), Space: 134 MB (11.28%) - LeetHub

---
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void Merge(vector<int>& nums, int l, int mid, int r){
+        vector<int> left;
+        vector<int> right;
+        for(int i=l;i<=mid;i++) left.push_back(nums[i]);
+        for(int i=mid+1;i<=r;i++) right.push_back(nums[i]);
+        
+        int f = 0, s = 0;
+        
+        while(f<left.size() && s<right.size()){
+            if(left[f] < right[s]){
+                nums[l] = left[f];
+                f++;
+            }
+            else{
+                nums[l] = right[s];
+                s++;
+            }
+            l++;
+        } 
+        
+        while(f<left.size()){
+            nums[l] = left[f];
+            l++;f++;
+        }
+        while(s<right.size()){
+            nums[l] = right[s];
+            s++;l++;
+        }
+    }
+    void mergeSort(vector<int>& nums, int l,int r){
+        if(l<r){
+            int mid = (l+r)/2;
+            
+            mergeSort(nums,l,mid);
+            mergeSort(nums,mid+1,r);
+            
+            Merge(nums,l,mid,r);
+        }
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums,0,nums.size()-1);
+        return nums;
+    }
+};
