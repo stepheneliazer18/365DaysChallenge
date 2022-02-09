fd369bd162c0c23aa834d7520a06a5b5cc065dd9
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-02-09 23:39:33 +0530

    Time: 175 ms (7.90%), Space: 11.1 MB (92.85%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -3,6 +3,7 @@ public:
     int findPairs(vector<int>& nums, int k) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
+        for(auto &it: nums) cout<<it<<" ";cout<<endl;
         int cnt = 0;
         if(k==0){
             for(int i=0;i<n-1;i++){
@@ -15,25 +16,25 @@ public:
         }
         
         
-        int low = 0;
-        while(low<n){
-            int c = nums[low] + k;
-            
-            int l = low+1;
-            int r = n-1;
-            
-            int mid;
-            while(l<=r){
-                mid = (l+r)/2;
-                if(nums[mid] == c){
-                    break;
-                }
-                else if(c < nums[mid]) r = mid-1;
-                else l = mid+1;
+        int i = 0;
+        int j = i+1;
+        
+        while(i<j && j<n){
+            cout<<i<<" "<<j<<endl;
+            int c = nums[j] - nums[i];
+            if(c == k){
+                cnt++;
+                i++;j++;
+                while(i<j && nums[i]==nums[i-1]) i++;
+                while(j<n && nums[j]==nums[j-1]) j++;
             }
-            if(nums[mid] == c && low<n-1 && nums[low]!=nums[low+1]) cnt++;
-            low++;
+            else if(c < k) j++;
+            else if(j == i+1){
+                j++;i++;
+            }
+            else i++;
         }
+        cout<<" -------- "<<endl;
         return cnt;
     }
 };
