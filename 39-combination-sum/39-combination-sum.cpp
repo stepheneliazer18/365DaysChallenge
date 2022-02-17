123c2278d770430783a0a881d13f6e1d0a2a64af
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-02-17 23:53:00 +0530

    Time: 381 ms (5.79%), Space: 79.5 MB (10.52%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    void help(vector<int>& cand, int ind, vector<int> vec, int target){
+        if(target < 0) return;
+        if(target == 0){
+            res.push_back(vec);
+            return;
+        }
+        if(ind == cand.size()) return;
+        
+        help(cand,ind+1,vec,target);
+        vec.push_back(cand[ind]);
+        help(cand,ind,vec,target-cand[ind]);
+    }
+    vector<vector<int>> combinationSum(vector<int>& cand, int target) {
+        vector<int> vec;
+        help(cand,0,vec,target);
+        
+        return res;
+    }
+};
