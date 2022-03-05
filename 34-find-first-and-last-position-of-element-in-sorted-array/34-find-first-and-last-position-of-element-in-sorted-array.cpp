4b2e439d0ef29debe15551b6ea558cda78cc160b
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-03-05 23:26:47 +0530

    Time: 19 ms (15.54%), Space: 13.7 MB (19.48%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,45 +1,25 @@
 class Solution {
 public:
+    int lower_bound(vector<int>& nums, int target){
+        int l = 0;
+        int r = nums.size()-1;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            
+            if(nums[mid] < target) l = mid+1;
+            else r = mid-1;
+        }
+        return l;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
         int n = nums.size();
         
-        vector<int> res{-1,-1};
-        if(n==0) return res;
+        int x1 = lower_bound(nums,target);
+        int x2 = lower_bound(nums,target+1) - 1;
         
-        int l = 0;
-        int r = n-1;
-        
-        int mid = 0;
-        while(l<r){
-            mid = (r+l)/2;
-            
-            if(nums[mid] == target){
-                break;
-            }
-            if(nums[l] == target){
-                mid = l;
-                break;
-            }
-            if(nums[r] == target){
-                mid = r;
-                break;
-            }
-            if(nums[mid] < target) l = mid+1;
-            else r = mid;
+        if(x1<nums.size() && nums[x1]==target){
+            return {x1,x2};
         }
-        cout<<mid<<endl;
-        if(nums[mid] == target && l<=r){
-            l = mid;
-            r = mid;
-            
-            while(nums[l]==target || nums[r]==target){
-                if(l-1 >= 0 && nums[l-1] == target) l--;
-                else if(r+1 < n && nums[r+1]==target) r++;
-                else break;
-            }
-            res[0] = l;
-            res[1] = r;
-        }
-        return res;
+        return {-1,-1};
     }
 };
