0c50fd696b16daec912fe0b77cd8de030b4330ca
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-03-06 21:25:44 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (29.56%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countOrders(int n) {
+        vector<long long> dp(501);
+        
+        int mod = pow(10,9) + 7;
+        
+        dp[1] = 1L;
+        dp[2] = 6L;
+        
+        for(int i=3;i<=n;i++){
+            int cnt = 2*i - 1;
+            int sum = (cnt)*(cnt+1)/2;
+            dp[i] = (dp[i-1]* sum)%mod;
+        }
+        
+        return (int) dp[n];
+    }
+};
