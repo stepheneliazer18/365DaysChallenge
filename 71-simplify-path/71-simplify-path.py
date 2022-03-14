404f1752a2eb67c5f44406ea7cf594fa5dc79c52
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-03-14 14:37:04 +0530

    Time: 70 ms (9.56%), Space: 14.2 MB (25.05%) - LeetHub

--- 71-simplify-path/71-simplify-path.py
+++ 71-simplify-path/71-simplify-path.py
@@ -3,8 +3,6 @@ class Solution:
         i = 0
         s = ''
         while(i<len(path)):
-            # print(path[i])
-            
             if path[i]=='/':
                 s+='/'
                 while (i<len(path)):
@@ -14,20 +12,14 @@ class Solution:
             else:
                 s+=path[i]
                 i+=1
-        print(s)
         l = s.split('/')
         if l[-1]=="": l.pop()
-        # print(l[1:])
         res = []
-        # bool_ = []
-        print(l)
         for i in range(len(l)):
             if l[i]=='.':
                 continue
             if l[i]=="..":
                 if res[-1]!="":res.pop()
-            # elif l[i]=="": continue
             else: res.append(l[i])
             print(res)
-        # print(res)
         return "/"+"/".join(res[1:])
