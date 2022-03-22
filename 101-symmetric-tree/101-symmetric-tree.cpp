a921d4e0dd584ec101ad5dd9cda36d7100f1fdc3
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-03-22 14:33:35 +0530

    Time: 16 ms (9.53%), Space: 16.4 MB (73.88%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -11,14 +11,22 @@
  */
 class Solution {
 public:
-    bool solve(TreeNode* p,TreeNode* q){
-        if(p==NULL || q==NULL) return p==q;
-
-        return p->val==q->val
-        && solve(p->left,q->right)
-        && solve(p->right,q->left);
+    
+    bool inorder(TreeNode* l, TreeNode* r){
+        if(!r && !l) return true;
+        if(!r) return false;
+        if(!l) return false;
+        
+        if(r->val != l->val) return false;
+        
+        bool le = inorder(l->left,r->right);
+        bool re = inorder(r->left,l->right);
+        
+        return le && re;
+        
     }
+    
     bool isSymmetric(TreeNode* root) {
-        return solve(root->left,root->right);
+        return inorder(root->left, root->right);
     }
 };
