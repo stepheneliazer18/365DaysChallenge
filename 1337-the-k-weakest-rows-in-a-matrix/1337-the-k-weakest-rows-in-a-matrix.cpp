9e4b15cf6a5b6ccba722d9332a5b09cae5a1f915
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-03-27 23:52:26 +0530

    Time: 17 ms (50.87%), Space: 10.5 MB (42.57%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -9,15 +9,9 @@ public:
         for(int i=0;i<m;i++){
             int cap = 0;
             for(auto &it: mat[i]) cap += it;
-            
-            cout<<"i = "<<i<<" cap = "<<cap<<endl;
             vec.push_back({cap,i});
         }
         sort(vec.begin(),vec.end());
-        
-        for(auto &it: vec){
-            cout<<it.first<<" "<<it.second<<endl;
-        }
         
         vector<int> res;
         for(int i=0;i<k;i++){
