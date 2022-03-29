6c0b14c7d9e50f35acb9d7eba1d498abfd1d3da4
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-03-29 10:54:41 +0530

    Time: 518 ms (37.31%), Space: 18.2 MB (67.58%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -1,18 +1,5 @@
 class Solution {
 public:
-    int help(int ind, int k, vector<vector<int>>& piles, vector<vector<int>>&lookup){
-        if(ind == -1 || k==0) return 0;
-        
-        if(lookup[ind][k] != -1) return lookup[ind][k];
-        int ans = help(ind-1,k,piles,lookup);
-        int sum = 0;
-        for(int i=0;i<min(k,(int)piles[ind].size());i++){
-            sum += piles[ind][i];
-            int temp = sum + help(ind-1,k-i-1,piles,lookup);
-            ans = max(ans,temp);
-        }
-        return lookup[ind][k] = ans;
-    }
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
         vector<vector<int>> dp(piles.size()+1,vector<int>(k+1,0));
         for(int ind = 0;ind<piles.size();ind++){
