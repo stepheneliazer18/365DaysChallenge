a80bcaf4c1bf2a3c73094991ed88cac74860788e
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-04-01 20:20:48 +0530

    Added solution - LeetHub

---
+++ Lucky Numbers - GFG/lucky-numbers.cpp
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User-function template for C++
+
+// Complete the function
+// n: Input n
+// Return True if the given number is a lucky number else return False
+
+class Solution{
+public:
+    int counter = 2;
+    bool fun(int n){
+        
+     
+        if(counter > n)
+            return 1;
+        if(n % counter == 0)
+            return 0;
+
+        int next_position = n - (n/counter);
+         
+        counter++;
+        return isLucky(next_position);
+    }
+    bool isLucky(int n){
+        return fun(n);
+    }
+};
+
+// { Driver Code Starts.
+signed main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int n;
+        cin>>n;
+        Solution obj;
+        //calling isLucky() function
+        if(obj.isLucky(n))
+            cout<<"1\n";//printing "1" if isLucky() returns true
+        else
+            cout<<"0\n";//printing "0" if isLucky() returns false
+    }
+    
+}  // } Driver Code Ends
