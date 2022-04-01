7d54d7f05c58dc3fc7ffbce6001ab9d3290cc6ae
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-04-01 23:42:09 +0530

    Time: 53 ms (52.35%), Space: 32.4 MB (62.52%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -11,35 +11,15 @@
  */
 class Solution {
 public:
-    void help(TreeNode* root,TreeNode* root1, TreeNode* root2){
-        if(!root1 && !root2) return;
-        if(root1 && root2) root->val = root1->val + root2->val;
-        else if(root1) root->val = root1->val;
-        else if(root2) root->val = root2->val;
-        
-        if(root1 && root1->left || root2 && root2->left){
-            root->left = new TreeNode();
-            if(root1 && root1->left && root2 && root2->left)
-                help(root->left,root1->left,root2->left);
-            else if(root1 && root1->left)
-                help(root->left,root1->left,NULL);
-            else if(root2 && root2->left)
-                help(root->left,NULL,root2->left);
-        }
-        if(root1 && root1->right || root2 &&  root2->right){
-            root->right = new TreeNode();
-            if(root1 && root1->right && root2 && root2->right)
-                help(root->right,root1->right,root2->right);
-            else if(root1 && root1->right)
-                help(root->right,root1->right,NULL);
-            else if(root2 && root2->right)
-                help(root->right,NULL,root2->right);
-        }
-    }
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        if(!root1 && !root2) return NULL;
-        TreeNode* root = new TreeNode();
-        help(root,root1,root2);
-        return root;
+        if(root1 && root2){
+            root1->val += root2->val;
+            root1->left = mergeTrees(root1->left,root2->left);
+            root1->right = mergeTrees(root1->right,root2->right);
+            return root1;
+        }
+        
+        if(root1) return root1;
+        return root2;
     }
 };
