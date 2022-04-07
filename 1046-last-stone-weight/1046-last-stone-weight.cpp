f5640beb831097b1ebebe66e33a4810e2eb57373
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-04-07 13:34:51 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (34.27%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        
+        for(auto &it: stones) pq.push(it);
+        
+        while(!pq.empty()){
+            if(pq.size() == 1){
+                return pq.top();
+            }
+            if(pq.size() == 2){
+                int n1 = pq.top();
+                pq.pop();
+                int n2 = pq.top();
+                pq.pop();
+                return abs(n1-n2);
+            }
+            int n1 = pq.top();
+            pq.pop();
+            int n2 = pq.top();
+            pq.pop();
+            
+            if(n1!=n2) pq.push(abs(n1-n2));
+        }
+        return 0;
+    }
+};
