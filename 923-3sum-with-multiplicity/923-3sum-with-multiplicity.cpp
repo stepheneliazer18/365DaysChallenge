d68102576716cbae59e0ccffe9d015292b06181b
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-04-08 12:56:50 +0530

    Time: 291 ms (52.48%), Space: 10.4 MB (88.49%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int threeSumMulti(vector<int>& nums, int target) {
-        long long n = nums.size();
+        int n = nums.size();
         sort(nums.begin(),nums.end());
         long long cnt = 0;
         
-        for(long long i=0;i<n;i++){
-            long long sum = target - nums[i];
-            long long l = i+1;
-            long long r = n-1;
+        for(int i=0;i<n;i++){
+            int sum = target - nums[i];
+            int l = i+1;
+            int r = n-1;
             while(l<r){
                 if(nums[l]+nums[r] == sum){
-                    long long z1 = 1,z2 = 1;
+                    int z1 = 1,z2 = 1;
                     if(nums[l] == nums[r]){
                         int x = r-l+1;
                         cnt += (x*(x-1))/2;
@@ -29,14 +29,9 @@ public:
                         }
                         cnt += z1*z2;
                     }
-
                 }
-                if(nums[l] + nums[r] < sum){
-                    l++;
-                }
-                else{
-                    r--;
-                }
+                if(nums[l] + nums[r] < sum) l++;
+                else r--;
             }
         }
         return cnt%1000000007;
