dab6897d5940d71dee855a47c161a985134f0db1
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-04-23 14:53:25 +0530

    Time: 18 ms (56.88%), Space: 13.6 MB (11.89%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void makeItZero(int x,int y, vector<vector<int>>& matrix){
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for(int i=0;i<n;i++) matrix[x][i] = 0;
+        for(int i=0;i<m;i++) matrix[i][y] = 0;
+    }
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> temp(m,vector<int>(n,-1));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j] == 0){
+                    cout<<i<<" "<<j<<endl;
+                    makeItZero(i,j,temp);
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(temp[i][j] == 0) matrix[i][j] = 0;
+            }
+        }
+    }
+};
