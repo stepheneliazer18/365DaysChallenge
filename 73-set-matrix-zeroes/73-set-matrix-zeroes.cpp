c01cbcdd5d70f4640e5fa1f4a53fff478322f897
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-04-23 15:31:31 +0530

    Time: 22 ms (35.49%), Space: 13.2 MB (82.52%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    void makeItZero(int x,int y, vector<vector<int>>& matrix){
-        int m = matrix.size();
-        int n = matrix[0].size();
-        for(int i=0;i<n;i++) matrix[x][i] = 0;
-        for(int i=0;i<m;i++) matrix[i][y] = 0;
-    }
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<vector<int>> temp(m,vector<int>(n,-1));
+        
+        bool rowBool = false;
+        bool colBool = false;
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j] == 0){
-                    makeItZero(i,j,temp);
+            if(matrix[i][0] == 0) colBool = true;
+            for(int j=1;j<n;j++){
+                if(i == 0 && matrix[i][j]==0){
+                    rowBool = true;
                 }
+                if(i>0 && matrix[i][j] == 0){
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }                
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(temp[i][j] == 0) matrix[i][j] = 0;
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i==0 && rowBool) matrix[i][j] = 0;
+                if(j==0 && colBool) matrix[i][j] = 0;
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j]=0;
             }
         }
     }
 };
-
-
-//Time Complexity: O(m x n + m x n)
-//Space Complexity: O(m x n)
