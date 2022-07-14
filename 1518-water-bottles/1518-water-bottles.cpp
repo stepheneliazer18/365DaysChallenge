9ee1de90d7ab1e6170e745592b7247eeced0cb91
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-07-15 00:34:04 +0530

    Time: 6 ms (5.45%), Space: 5.9 MB (23.36%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.cpp
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
 	int numWaterBottles(int nbs, int neb) {
 		long long sum=nbs;
-		while(nbs/neb>0)
-		{
+		while(nbs/neb>0){
 			sum+=nbs/neb;
 			int r=nbs%neb;
 			nbs=nbs/neb;
