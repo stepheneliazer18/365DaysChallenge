4a433e119945aeb01acd6fb6b4719f7d3dbc9f3a
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-07-15 11:44:47 +0530

    Time: 79 ms (20.58%), Space: 18.4 MB (42.13%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -9,7 +9,6 @@ public:
         
         if(dp[ind][target] != -1) return dp[ind][target];
         
-        
         int not_take = solve(ind-1,target,coins,dp);
         int take = 0;
         if(coins[ind] <= target) take = solve(ind,target-coins[ind],coins,dp);
@@ -18,6 +17,21 @@ public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
         vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        return solve(n-1,amount,coins,dp);
+        // return solve(n-1,amount,coins,dp);
+        
+        for(int target=0;target<=amount;target++){
+            if(target % coins[0] == 0) dp[0][target] = 1;
+            else dp[0][target] = 0;
+        }
+        
+        for(int ind=1;ind<n;ind++){
+            for(int target=0;target<=amount;target++){
+                int not_take = dp[ind-1][target];
+                int take = 0;
+                if(coins[ind] <= target) take = dp[ind][target-coins[ind]];
+                dp[ind][target] = not_take + take;
+            }
+        }
+        return dp[n-1][amount];
     }
 };
