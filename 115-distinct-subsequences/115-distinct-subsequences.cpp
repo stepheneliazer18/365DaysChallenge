a1f1f94829e0ceff79765c6dda5d35ff61f875bb
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-07-17 00:13:27 +0530

    Time: 51 ms (62.60%), Space: 18.5 MB (47.60%) - LeetHub

---
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int solve(int ind1, int ind2, string &s1, string &s2, vector<vector<int>>& dp){
+        if(ind2 < 0) return 1;
+        if(ind1 < 0) return 0;
+        
+        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+        
+        if(s1[ind1] == s2[ind2]){
+            int moveBoth = solve(ind1-1,ind2-1,s1,s2,dp);
+            int moveFirst = solve(ind1-1,ind2,s1,s2,dp);
+            return dp[ind1][ind2] = moveBoth + moveFirst;
+        }
+        
+        return dp[ind1][ind2] = solve(ind1-1,ind2,s1,s2,dp);
+    }
+    int numDistinct(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+        
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return solve(n-1,m-1,s,t,dp);
+    }
+};
