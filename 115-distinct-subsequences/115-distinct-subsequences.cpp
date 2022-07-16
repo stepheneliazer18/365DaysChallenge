f5a576e03964dd798459385e76ae3eb27ce64a3e
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-07-17 00:20:56 +0530

    Time: 102 ms (12.38%), Space: 18.5 MB (47.60%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    int mod = 1e9+7;
     int solve(int ind1, int ind2, string &s1, string &s2, vector<vector<int>>& dp){
         if(ind2 == 0) return 1;
         if(ind1 == 0) return 0;
@@ -14,11 +15,28 @@ public:
         
         return dp[ind1][ind2] = solve(ind1-1,ind2,s1,s2,dp);
     }
-    int numDistinct(string s, string t) {
-        int n = s.length();
-        int m = t.length();
+    int numDistinct(string s1, string s2) {
+        int n = s1.length();
+        int m = s2.length();
         
         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return solve(n,m,s,t,dp);
+        // return solve(n,m,s,t,dp);
+        
+        for(int ind1=0;ind1<=n;ind1++) dp[ind1][0] = 1;
+        for(int ind2=1;ind2<=m;ind2++) dp[0][ind2] = 0;
+        
+        for(int ind1=1;ind1<=n;ind1++){
+            for(int ind2=1;ind2<=m;ind2++){
+                if(s1[ind1-1] == s2[ind2-1]){
+                    int moveBoth = dp[ind1-1][ind2-1];
+                    int moveFirst = dp[ind1-1][ind2];
+                    dp[ind1][ind2] = (moveBoth + moveFirst) % mod;
+                }
+                else{
+                    dp[ind1][ind2] = dp[ind1-1][ind2];
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
