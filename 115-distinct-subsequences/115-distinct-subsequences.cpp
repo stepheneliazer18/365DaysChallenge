86b708dcce2af8fa2df66d5b02d3ca700d56f030
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-07-17 00:23:45 +0530

    Time: 61 ms (48.06%), Space: 18.1 MB (66.08%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -19,24 +19,28 @@ public:
         int n = s1.length();
         int m = s2.length();
         
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        // vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
         // return solve(n,m,s,t,dp);
         
-        for(int ind1=0;ind1<=n;ind1++) dp[ind1][0] = 1;
-        for(int ind2=1;ind2<=m;ind2++) dp[0][ind2] = 0;
+        vector<int> prev(m+1,0);
+        
+        prev[0] = 1;
         
         for(int ind1=1;ind1<=n;ind1++){
+            vector<int> cur(m+1,0);
+            cur[0]=1;
             for(int ind2=1;ind2<=m;ind2++){
                 if(s1[ind1-1] == s2[ind2-1]){
-                    int moveBoth = dp[ind1-1][ind2-1];
-                    int moveFirst = dp[ind1-1][ind2];
-                    dp[ind1][ind2] = (moveBoth + moveFirst) % mod;
+                    int moveBoth = prev[ind2-1];
+                    int moveFirst = prev[ind2];
+                    cur[ind2] = (moveBoth + moveFirst) % mod;
                 }
                 else{
-                    dp[ind1][ind2] = dp[ind1-1][ind2];
+                    cur[ind2] = prev[ind2];
                 }
             }
+            prev = cur;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
