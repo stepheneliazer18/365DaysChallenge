684185a26d55c37b59376f4af58cccff11b36387
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-08-05 23:15:40 +0530

    Time: 108 ms (42.78%), Space: 7 MB (88.51%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -24,10 +24,13 @@ public:
         int n = s1.size();
         int m = s2.size();
         
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        // vector<vector<int>> dp(n+1,vector<int>(m+1,0));
         // return solve(n-1,m-1,s1,s2,dp);
         
-        dp[0][0] = true;
+        vector<int> prev(m+1,0);
+        vector<int> cur(m+1,0);
+        
+        prev[0] = true;
         for(int j=1;j<=m;j++){
             bool ans = true;
             for(int x=j;x>0;x--){
@@ -36,17 +39,18 @@ public:
                     break;
                 }
             }
-            dp[0][j] = ans;
+            prev[j] = ans;
         }
-        for(int i=1;i<=n;i++) dp[i][0] = false;
         
         for(int i=1;i<=n;i++){
+            cur[0] = false;
             for(int j=1;j<=m;j++){
-                if(s1[i-1] == s2[j-1] || s2[j-1] == '?') dp[i][j] = dp[i-1][j-1];
-                else if(s2[j-1] == '*') dp[i][j] = dp[i][j-1] || dp[i-1][j];
-                else dp[i][j] = false;
+                if(s1[i-1] == s2[j-1] || s2[j-1] == '?') cur[j] = prev[j-1];
+                else if(s2[j-1] == '*') cur[j] = cur[j-1] || prev[j];
+                else cur[j] = false;
             }
+            prev = cur;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
