10c7c0010be53f44a2e433eba942e73d8fafb886
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-08-05 20:51:19 +0530

    Time: 48 ms (23.21%), Space: 9 MB (57.72%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -21,7 +21,20 @@ public:
         int n = s1.size();
         int m = s2.size();
         
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,s1,s2,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        // return solve(n-1,m-1,s1,s2,dp);
+        
+        for(int i=1;i<=n;i++) dp[i][0] = i;
+        for(int j=1;j<=m;j++) dp[0][j] = j;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1] == s2[j-1]) dp[i][j] = dp[i-1][j-1];
+                else{
+                    dp[i][j] = 1 + min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
