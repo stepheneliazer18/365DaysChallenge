3f1c5a0e5c46d31bacf0e26286dc65001aac8920
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-08-05 23:08:46 +0530

    Time: 77 ms (63.88%), Space: 28 MB (13.37%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -24,7 +24,29 @@ public:
         int n = s1.size();
         int m = s2.size();
         
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,s1,s2,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        // return solve(n-1,m-1,s1,s2,dp);
+        
+        dp[0][0] = true;
+        for(int j=1;j<=m;j++){
+            bool ans = true;
+            for(int x=j;x>0;x--){
+                if(s2[x-1] != '*'){
+                    ans = false;
+                    break;
+                }
+            }
+            dp[0][j] = ans;
+        }
+        for(int i=1;i<=n;i++) dp[i][0] = false;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1] == s2[j-1] || s2[j-1] == '?') dp[i][j] = dp[i-1][j-1];
+                else if(s2[j-1] == '*') dp[i][j] = dp[i][j-1] || dp[i-1][j];
+                else dp[i][j] = false;
+            }
+        }
+        return dp[n][m];
     }
 };
