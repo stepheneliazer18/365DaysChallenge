1bad4a89614f611337b0279da0537e86ce313b96
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-08-17 10:38:02 +0530

    Time: 19 ms (52.85%), Space: 13.5 MB (27.13%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
@@ -1,39 +1,34 @@
 bool cmp(pair<string,string> p1, pair<string,string> p2){
-    // if logs are equal we compare identifiers
-    if(p1.second == p2.second) return p1.first < p2.first;
-    return p1.second < p2.second;
+    if(p1.first == p2.first) return p1.second < p2.second;
+    return p1.first < p2.first;
 }
 class Solution {
 public:
+    
     vector<string> reorderLogFiles(vector<string>& logs) {
         vector<pair<string,string>> vec1;
         vector<string> vec2;
-        
-        // we are taking letter logs into vec1
-        // and digit logs into vec2 as we don't have to modify digit logs
-        // we just have to sort letter logs and append the digit logs at the end
         
         for(auto &it: logs){
             string identifier, logs;
             
             int i = 0;
             int n = it.length();
-            while(it[i] != ' ') identifier += it[i++];  // separating identifier and logs
+            while(it[i] != ' ') identifier += it[i++];
             i++;
             while(i<n) logs += it[i++];
             
-            if(isdigit(logs[0])) vec2.push_back(it);  // digit logs    
-            else vec1.push_back({identifier,logs});   // letter logs
+            if(isdigit(logs[0])) vec2.push_back(it);
+            else vec1.push_back({logs,identifier});
         }
-              
-        // sorting the letter logs using comparator function 
+                
         sort(vec1.begin(),vec1.end(),cmp);
         
         vector<string> res;
         for(auto &it: vec1){
-            res.push_back(it.first + " " +it.second);//storing the sorted letter logs into res 
+            res.push_back(it.second + " " + it.first);
         }
-        for(auto &it: vec2) res.push_back(it); // appending the digit logs at the end of res
+        for(auto &it: vec2) res.push_back(it);
         
         return res;
     }
