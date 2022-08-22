d1080adb264eff48d75f8f3f79a1bf8592eaf3a6
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-08-23 00:28:29 +0530

    Time: 6 ms (37.87%), Space: 6.7 MB (5.14%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence){
+        map<char, int> mp;
+        for(int i=0; i<sentence.size();i++){
+            mp[sentence[i]]++;
+        }
+        if(mp.size() == 26){
+            return true;
+        }
+        return false;
+    }
+};
