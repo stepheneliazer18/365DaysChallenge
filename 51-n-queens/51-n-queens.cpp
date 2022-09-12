51dc85f9664e211a0e10b9dd34f3997c61cbab89
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-09-12 12:23:57 +0530

    Time: 4 ms (89.93%), Space: 7.3 MB (53.06%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,41 +1,22 @@
 class Solution {
 public:
-    bool isvalid(int row, int col, vector<string>& output){
-        int i = row;
-        int j = col;
-
-        // left
-        while(j >= 0){
-            if(output[row][j] == 'Q') return false;
-            j--;
-        }
-        
-        // left up diagonal
-        i = row;
-        j = col;
-        while(row >= 0 && col >= 0){
-            if(output[row][col] == 'Q') return false;
-            row--;col--;
-        }
-        
-        // left down diagonal
-        while(i < output.size() && j >= 0){
-            if(output[i][j] == 'Q') return false;
-            i++;j--;
-        }
-        
-        return true;
-    }
-    void nqueen(int col, vector<string>& output, vector<vector<string>>& ans){
-        if(col == output.size()){
+    void nqueen(int col, vector<string>& output, vector<vector<string>>& ans, vector<int>& leftRow, vector<int>& lowerDiagonal, vector<int>& upperDiagonal){
+        int n = output.size();
+        if(col == n){
             ans.push_back(output);    
         }
         
         for(int row=0;row<output.size();row++){
-            if(isvalid(row,col,output)){
+            if(leftRow[row] == 0 && lowerDiagonal[row+col] == 0 && upperDiagonal[n-1+col-row] == 0){
+                leftRow[row] = 1;
+                lowerDiagonal[row+col] = 1;
+                upperDiagonal[n-1+col-row] = 1;
                 output[row][col] = 'Q';
-                nqueen(col+1,output,ans);
+                nqueen(col+1,output,ans,leftRow,lowerDiagonal,upperDiagonal);
                 output[row][col] = '.';
+                upperDiagonal[n-1+col-row] = 0;
+                lowerDiagonal[row+col] = 0;
+                leftRow[row] = 0;
             }
         }
     }
@@ -43,7 +24,10 @@ public:
         vector<vector<string>> ans;
         string s(n,'.');
         vector<string> output(n,s);
-        nqueen(0,output,ans);
+        vector<int> leftRow(n,0);
+        vector<int> upperDiagonal(2*n-1,0);
+        vector<int> lowerDiagonal(2*n-1,0);
+        nqueen(0,output,ans,leftRow,lowerDiagonal,upperDiagonal);
         return ans;
     }
 };
