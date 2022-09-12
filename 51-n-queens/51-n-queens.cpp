31e90b5e37bcc1faf00528d774271d94c8c66be1
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-09-12 11:58:56 +0530

    Time: 24 ms (28.54%), Space: 7.3 MB (66.08%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool isvalid(int row, int col, vector<string>& output){
+        int i = row;
+        int j = col;
+        
+        // up
+        while(i >= 0){
+            if(output[i][col] == 'Q') return false;
+            i--;
+        }
+        
+        // left
+        while(j >= 0){
+            if(output[row][j] == 'Q') return false;
+            j--;
+        }
+        
+        // left up diagonal
+        i = row;
+        j = col;
+        while(row >= 0 && col >= 0){
+            if(output[row][col] == 'Q') return false;
+            row--;col--;
+        }
+        
+        // left down diagonal
+        while(i < output.size() && j >= 0){
+            if(output[i][j] == 'Q') return false;
+            i++;j--;
+        }
+        
+        return true;
+    }
+    void nqueen(int col, vector<string>& output, vector<vector<string>>& ans){
+        if(col == output.size()){
+            ans.push_back(output);    
+        }
+        
+        for(int row=0;row<output.size();row++){
+            if(isvalid(row,col,output)){
+                output[row][col] = 'Q';
+                nqueen(col+1,output,ans);
+                output[row][col] = '.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        string s(n,'.');
+        vector<string> output(n,s);
+        nqueen(0,output,ans);
+        return ans;
+    }
+};
