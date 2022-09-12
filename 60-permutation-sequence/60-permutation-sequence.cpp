4418d115bef992f4df9643c9e8709e006161b3db
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-09-12 23:59:32 +0530

    Time: 8 ms (26.94%), Space: 5.8 MB (95.51%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     string getPermutation(int n, int k) {
         vector<int> nums;
-        for(int i=1;i<=n;i++) nums.push_back(i);
         int total = 1;
-        for(int i=n;i>=1;i--) total *= i;
+        for(int i=1;i<=n;i++){
+            nums.push_back(i);
+            total *= i;
+        }
         
         string ans;
-        int size = n;
         k--;
         for(int i=0;i<n;i++){
-            size = total/nums.size();
+            int size = total/nums.size();
             int ind = k/size;
             ans += (nums[ind] + '0');
             nums.erase(nums.begin() + ind);
