270d6d3deaeb8a0d45cda6e2bfd55a353b6e24b7
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-09-14 17:03:52 +0530

    Time: 12 ms (53.33%), Space: 13.2 MB (32.95%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -7,42 +7,41 @@ public:
         vector<int> dx{0,1,0,-1};
         vector<int> dy{1,0,-1,0};
         
-        queue<pair<int,int>> q;
+        queue<pair<pair<int,int>,int>> q;
         
         int oranges = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j] == 1) oranges++;
                 if(grid[i][j] == 2){
-                    q.push({i,j}); 
+                    q.push({{i,j},0}); 
                 }
             }
         }
         
-        int cnt = -1;
+        int maxTime = 0;
         while(!q.empty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                int nodex = q.front().first;
-                int nodey = q.front().second;
-                q.pop();
-                
-                for(int i=0;i<4;i++){
-                    int nr = nodex + dx[i];
-                    int nc = nodey + dy[i];
-                    
-                    if(nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1){
-                        grid[nr][nc] = 2;
-                        oranges--;
-                        q.push({nr,nc});
-                    }
+            int nodex = q.front().first.first;
+            int nodey = q.front().first.second;
+            int time = q.front().second;
+            q.pop();
+            
+            maxTime = max(maxTime,time);
+
+            for(int i=0;i<4;i++){
+                int nr = nodex + dx[i];
+                int nc = nodey + dy[i];
+
+                if(nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1){
+                    grid[nr][nc] = 2;
+                    oranges--;
+                    q.push({{nr,nc},time+1});
                 }
-            }   
-            cnt++;
+            }  
         }
         
         if(oranges) return -1;
-        return cnt == -1 ? 0 : cnt;
+        return maxTime;
     }
 };
 
