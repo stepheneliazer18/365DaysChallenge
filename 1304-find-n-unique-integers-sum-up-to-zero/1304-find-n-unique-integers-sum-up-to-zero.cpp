3b1fb1c566ddcd798b606b2bca51dd7c254ca7de
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-09-19 00:17:25 +0530

    Time: 3 ms (51.32%), Space: 6.7 MB (51.39%) - LeetHub

---
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> ans;
+        if(n%2) ans.push_back(0);
+        for(int i=1;i<=n/2;i++){
+            ans.push_back(i);
+            ans.push_back(-i);
+        }
+        return ans;
+    }
+};
