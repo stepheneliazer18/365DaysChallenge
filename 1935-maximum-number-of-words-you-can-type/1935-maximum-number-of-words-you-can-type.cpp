487771caff57e896276b008eb414b7036842b226
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-09-28 23:08:55 +0530

    Time: 4 ms (64.61%), Space: 6.6 MB (61.36%) - LeetHub

---
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int canBeTypedWords(string text, string bl) {
+        int len = text.length();
+        int count = 0;
+        int i = 0; 
+        bool t = false;
+        unordered_set<char>mp;
+        for(auto &it: bl) mp.insert(it);
+
+        while(i<=text.size()){
+            if(mp.count(text[i])) t=true;
+            else  if(text[i]==' '||text[i]=='\0'){
+                if(!t)
+                count++;
+                t=false;
+            }
+            i++;
+        }
+
+        return count;
+    }
+};
