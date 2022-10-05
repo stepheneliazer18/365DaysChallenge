8c5d734117594532f18ee01cf4582d42b2b386aa
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-10-05 23:51:18 +0530

    Time: 10 ms (10.03%), Space: 6.4 MB (82.48%) - LeetHub

---
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+	int minimumMoves(string s) {
+		int n = s.size();
+		int ans = 0;
+		for(int i=0;i<n;i++){
+			if(s[i] == 'X'){
+				ans++;
+				i+=2;
+			}
+		}
+		return ans;
+	}
+};
