a7c8a799e23a09287a600df7bf0839edde53a53e
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-10-14 12:38:42 +0530

    Added solution - LeetHub

--- Shortest Distance in a Binary Maze - GFG/shortest-distance-in-a-binary-maze.cpp
+++ Shortest Distance in a Binary Maze - GFG/shortest-distance-in-a-binary-maze.cpp
@@ -16,7 +16,6 @@ class Solution {
   public:
     int shortestPath(vector<vector<int>> &grid, pair<int, int> source,
                      pair<int, int> destination) {
-        if(source == destination) return 0;
         int n = grid.size();
         int m = grid[0].size();
         
@@ -24,33 +23,24 @@ class Solution {
         vector<int> dx{0,1,0,-1};
         
         vector<vector<int>> distance(n,vector<int>(m,INT_MAX));
-        // priority_queue<int, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
-        set<pair<int,pair<int,int>>> st;
-        
-        // pq.push({0,source});
-        st.insert({0,source});
+        queue<pair<int,pair<int,int>>> q;
+
+        q.push({0,source});
         distance[source.first][source.second] = 0;
         
-        while(!st.empty()){
-            // int dist = pq.top().first;
-            // int x = pq.top().second.first;
-            // int y = pq.top().second.second;
-            // pq.pop();
-            
-            auto node = *(st.begin());
+        while(!q.empty()){
+            auto node = q.front();
             int dist = node.first;
             int x = node.second.first;
             int y = node.second.second;
-            st.erase(node);
+            q.pop();
             
             for(int i=0;i<4;i++){
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 
                 if(isvalid(nx,ny,n,m) && grid[nx][ny] && dist + 1 < distance[nx][ny]){
-                    // pq.push({dist+1,{nx,ny}});
-                    if(distance[nx][ny] != INT_MAX) st.erase({distance[nx][ny],{nx,ny}});
-                    st.insert({dist+1,{nx,ny}});
+                    q.push({dist+1,{nx,ny}});
                     distance[nx][ny] = dist+1;
                 }
             }
@@ -60,7 +50,6 @@ class Solution {
         return ans;
     }
 };
-
 
 //{ Driver Code Starts.
 int main() {
