698d08b788a554a61c1f3da2f90bcb633c573d30
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-10-15 15:46:29 +0530

    Added solution - LeetHub

---
+++ Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
+        vector<int> distance(V,1e8);
+        distance[S] = 0;
+        
+        for(int i=1;i<V;i++){
+            for(auto &it: edges){
+                int u = it[0];
+                int v = it[1];
+                int w = it[2];
+                
+                if(distance[u] != 1e8 && distance[u] + w < distance[v]){
+                    distance[v] = distance[u] + w;
+                }
+            }
+        }
+        
+        vector<int> temp = distance;
+        for(auto &it: edges){
+            int u = it[0];
+            int v = it[1];
+            int w = it[2];
+            
+            if(distance[u] != 1e8 && distance[u] + w < distance[v]){
+                distance[v] = distance[u] + w;
+            }
+        }
+        
+        if(temp != distance) return {-1};
+        return distance;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, m;
+        cin >> N >> m;
+        vector<vector<int>> edges;
+
+        for (int i = 0; i < m; ++i) {
+            vector<int> temp;
+            for (int j = 0; j < 3; ++j) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            edges.push_back(temp);
+        }
+
+        int src;
+        cin >> src;
+
+        Solution obj;
+        vector<int> res = obj.bellman_ford(N, edges, src);
+
+        for (auto x : res) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
