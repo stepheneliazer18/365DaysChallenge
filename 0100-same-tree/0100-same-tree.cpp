9da2cdfcb35e36510d2a14ebe02eb75c8894c91d
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-10-20 15:33:57 +0530

    Time: 3 ms (69.92%), Space: 10 MB (77.40%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -16,7 +16,10 @@ public:
         if(!p || !q) return false;
         if(p->val != q->val) return false;
         
-        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+        bool left = isSameTree(p->left,q->left);
+        bool right = isSameTree(p->right,q->right);
+        
+        return left && right;
         
     }
 };
