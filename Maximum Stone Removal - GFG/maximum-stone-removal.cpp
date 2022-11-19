f8ff15cd91f8d78016aa422bcd6903e15021056d
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2022-11-19 21:23:01 +0530

    Added solution - LeetHub

---
+++ Maximum Stone Removal - GFG/maximum-stone-removal.cpp
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class DisjointSet{
+public:
+    vector<int> size, parent;
+    DisjointSet(int n){
+        size.resize(n+1,1);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i] = i;
+        }
+    }
+    int findUPar(int node){
+        if(node == parent[node]){
+            return node;
+        }
+        return parent[node] = findUPar(parent[node]);
+    }
+    void unionBySize(int u, int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v) return;
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+class Solution {
+  public:
+    int maxRemove(vector<vector<int>>& stones, int s) {
+        int n = 0, m = 0;
+        
+        for(auto &it: stones){
+            n = max(n,it[0]);
+            m = max(m,it[1]);
+        }
+        n++,m++;
+        
+        DisjointSet ds(n+m);
+        for(auto &it: stones){
+            int rowNode = it[0];
+            int colNode = n + it[1];
+            
+            ds.unionBySize(rowNode, colNode);
+        }
+        
+        int connectedComponents = 0;
+        for(int i=0;i<n+m;i++){
+            if(ds.parent[i] == i && ds.size[i] > 1) connectedComponents++;
+        }
+        
+        return s - connectedComponents;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> adj;
+
+        for (int i = 0; i < n; ++i) {
+            vector<int> temp;
+            for (int i = 0; i < 2; ++i) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
+
+        Solution obj;
+
+        cout << obj.maxRemove(adj, n) << "\n";
+    }
+}
+// } Driver Code Ends
