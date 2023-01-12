1741b939ebe55d9628cde9aa2f04ec1400fc36b9
Stephen Eliazer <95648419+stepheneliazer18@users.noreply.github.com>
2023-01-13 00:07:30 +0530

    Added solution - LeetHub

--- Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
@@ -7,13 +7,13 @@ using namespace std;
 class Solution{
 public:
     int *findTwoElement(int *arr, int N) {
-        long long int n = N;
+        long long  n = N;
         long long s = (n*(n+1))/2 * (1LL);
         long long p = n*((n+1)*((2*n) + 1))/6 * (1LL);
         
         for(int i=0;i<n;i++){
-            s -= (long long int)arr[i];
-            p -= (long long int)arr[i] * (long long int)arr[i];
+            s -= (long long )arr[i];
+            p -= (long long )arr[i] * (long long )arr[i];
         }
         
         int *a = new int[2];
